02793a734de2d68e6a3177fab6e0dd1c77599ca8
PodTatoHead - CI <demo@keptn.sh>
2022-01-22 12:33:16 +0100

    Update service hats to version distroless-v1-latest-dev

--- base/hats/k6/smoke.js
+++ base/hats/k6/smoke.js
@@ -11,7 +11,7 @@ export let options = {
 };
 
 export default function () {
-  const res = http.get('http://podtato-hats-preview.podtatohead-dev.svc.cluster.local:9000');
+  const res = http.get('http://podtato-${__ENV.SERVICE}-preview.podtatohead-${__ENV.STAGE}.svc.cluster.local:8080');
   const result = check(res, {
     'status is 200': (r) => r.status == 200,
   });
